5a17cd2b2a4f512e40aecd208823707357caec66
Ashrafur Siddiqui <siddiqua@bxscience.edu>
2021-05-09 15:58:33 -0400

    Added more descriptors and a image preview to mango page

--- README.md
+++ README.md
@@ -2,4 +2,16 @@
 
 On March 21st, 2021, anons lost their access to the greatest mango distro, mangodex
 
-Today we bring it back
+Today we bring it back.
+
+# Changelog
+
+## 2021-09-05
+### Added
+Gave basic browsing capabilites of the mangodex's catalogue, as well support for reading chapters and viewing a mango page.
+### TODO: 
+Searching by title, author, artist. 
+Adding author and artist to respective mango page. 
+Go to next and previous chapter buttons.
+Searching by multiple tags.
+Getting recently updated manga with tag support.
--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 
 import os,json,requests
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 from collections import OrderedDict
 
 app = Flask(__name__)
@@ -28,6 +28,15 @@ def view_mango(mango_hash):
     """
     url = base_url + "manga/" + mango_hash + "/feed?locales[]=en"
     return url
+
+def mango_meta(mango_hash):
+    """
+    returns the url to get descriptor for a given mango
+    """
+    url = base_url + "manga/" + mango_hash
+    return url
+
+
 def get_server(chap_id):
     """
     returns the url to server to retrieve chap from
@@ -46,7 +55,9 @@ def get_pages(chap_hash):
 
 def get_images(server,chap_hash,images):
     """
-    returns an array of the urls in a given chapter when provided the mangodex at home server, chapter hash and the file names of the images
+    returns an array of the urls in a given chapter when provided the mangodex at home server, 
+    chapter hash and the file names of the images
+    Make sure to pass in the data-saver array for the images
     """
     chapter=[]
     url = server+"/data/"+chap_hash
@@ -54,6 +65,17 @@ def get_images(server,chap_hash,images):
         page=url+'/'+i
         chapter.append(page)
     return chapter
+
+def get_img_preview(server,chap_hash,images,m=1):
+    """
+    returns a url for the [m] image of the chapter when provided
+    the mangodex at home server, chapter hash and the file names of the images
+    """
+
+    url = server+"/data-saver/"+chap_hash
+    page=url+'/'+images[m]
+
+    return page
 
 f = open("tags.json", "r")
 
@@ -90,8 +112,20 @@ def mango_no_tag(offset):
 @app.route('/mango_page/<string:mango_hash>')
 def mango_page(mango_hash):
     url = view_mango(mango_hash)
+    #data for chapter 
     result = requests.request("GET", url).json()['results']
-    return render_template("mango.html", results = result)
+
+    #meta data for the manga
+    meta = requests.request("GET", mango_meta(mango_hash)).json()
+    print(mango_meta(mango_hash))
+    #image preview for mango (generally page two of the first chapter)
+    #use data saver version
+    preview = 0
+    if len(result) > 0:
+        chapter1 = result[-1]['data']
+        server = get_server(chapter1['id'])
+        preview = get_img_preview(server,chapter1['attributes']['hash'],chapter1['attributes']['dataSaver'] ,m=0)
+    return render_template("mango.html", results = result, meta=meta, preview=preview)
 
 @app.route('/chapter/<string:chap_id>')
 def chapter_loader(chap_id):
@@ -103,10 +137,14 @@ def chapter_loader(chap_id):
 
 @app.route('/tags')
 def tags():
-    
     return render_template("tag.html",tags=tag_data)
+
+@app.route('/test')
+def adhoc_test():
+    return request.query_string
 
 
 if __name__ == '__main__':
   port = int(os.environ.get('PORT', 5000))
-  app.run(host = '0.0.0.0', port = port)
+  app.run(debug=True)
+  #app.run(host = '0.0.0.0', port = port)
