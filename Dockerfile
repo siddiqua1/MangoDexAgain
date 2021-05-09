4a20b38e772eb0ebde67595e468a7d915ac9a0fc
Ashrafur Siddiqui <siddiqua@bxscience.edu>
2021-05-09 17:35:50 -0400

    added environment variable to toggle local dev

--- Dockerfile
+++ Dockerfile
@@ -3,6 +3,8 @@ FROM python:3.8.1
 ENV APP_HOME /app
 WORKDIR $APP_HOME
 
+ENV PROD=true
+
 COPY . /app
 
 RUN pip install -r requirements.txt
--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import Flask, render_template, request
 from collections import OrderedDict
 
 app = Flask(__name__)
+prod = os.getenv("PROD", 'False').lower() in ('true', '1', 't')
 
 base_url = "https://api.mangadex.org/"
 # Append the chapter uuid to get a new url for hosting that chapter
@@ -149,6 +150,8 @@ def adhoc_test():
 
 
 if __name__ == '__main__':
-  port = int(os.environ.get('PORT', 5000))
-  #app.run(debug=True)
-  app.run(host = '0.0.0.0', port = port)
+    if prod:
+        port = int(os.environ.get('PORT', 5000))
+        app.run(host = '0.0.0.0', port = port)
+    else: 
+        app.run(debug=True)
